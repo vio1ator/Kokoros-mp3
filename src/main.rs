1778be2ab635a596dcc2c91b47e08fca0e77e008
Your Name <you@example.com>
2025-01-15 19:45:37 +0800

    placeholder of openai like server interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod onn;
+mod serve;
 mod tts;
 mod utils;
 
@@ -12,21 +13,28 @@ use tts::koko::TTSKoko;
 struct Cli {
     #[arg(short = 't', long = "text", value_name = "TEXT")]
     text: Option<String>,
+
+    #[arg(long = "oai", value_name = "OpenAI server")]
+    oai: bool,
 }
 
 fn main() {
     let args = Cli::parse();
 
-    let txt = args.text.unwrap_or_else(|| {
-        r#"
-        Hello, This is Kokoro. Your amazing AI TTS! A TTS model with only 82 million parameters that achieve incredible audio quality. 
-        This is the one of the best Rust inference, I help you will like it. 
-        Please give us a star if you do, thank you very much.
-        "#
-        .to_string()
-    });
+    if args.oai {
+        println!("OpenAI like server still need help!")
+    } else {
+        let txt = args.text.unwrap_or_else(|| {
+            r#"
+            Hello, This is Kokoro. Your amazing AI TTS! A TTS model with only 82 million parameters that achieve incredible audio quality. 
+            This is the one of the best Rust inference, I help you will like it. 
+            Please give us a star if you do, thank you very much.
+            "#
+            .to_string()
+        });
 
-    let tts = TTSKoko::new("checkpoints/kokoro-v0_19.onnx");
+        let tts = TTSKoko::new("checkpoints/kokoro-v0_19.onnx");
 
-    tts.tts(&txt, "en-us", "af_sky");
+        tts.tts(&txt, "en-us", "af_sky");
+    }
 }
