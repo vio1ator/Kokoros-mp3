4701d8e45132e112440e4e6fe19ea7319e8a3608
Your Name <you@example.com>
2025-01-15 12:59:07 +0800

    fully inference workable

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ mod onn;
 mod tts;
 mod utils;
 
+use tts::koko::TTSKoko;
+
 fn main() {
-    let tts = tts::koko::TTSKoko::new("checkpoints/kokoro-v0_19.onnx");
-    tts.tts(
-        "Hello from Kokoro, your amazing AI TTS!",
-        "en-us",
-        "af_bella",
-    );
+    let tts = TTSKoko::new("checkpoints/kokoro-v0_19.onnx");
+
+    let txt = r#"
+    Hello, This is Kokoro. Your amazing AI TTS! A TTS model with only 82 million parameters that achieve incredable audio quality. 
+    This is the one of the best Rust inference, I help you will like it. 
+    Please give us a star if you do, thank you very much.
+    "#;
+    tts.tts(txt, "en-us", "bf_emma");
 }
